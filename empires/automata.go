a1f417a44fe0a623748295ef3b9a78ff6dc4715a
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-04-16 21:53:10 -0700

    Make code slightly more performant

--- empires/automata.go
+++ empires/automata.go
@@ -11,21 +11,23 @@ func simulatePos(x, y int) {
 	if out[y][x].Color != r.Blank {
 		return
 	}
-	if (frame[y][x].Color == groundColor) || (frame[y][x].Color == waterColor) {
-		out[y][x] = frame[y][x]
+
+	me := frame[y][x]
+	if (me.Color == groundColor) || (me.Color == waterColor) {
+		out[y][x] = me
 		return
 	}
 
 	// Aging
 	frame[y][x].Age++
 	frame[y][x].ReproductionValue++
-	if frame[y][x].Age > frame[y][x].Strength {
+	if me.Age > me.Strength {
 		out[y][x].Color = groundColor
 		frame[y][x].Color = groundColor
 		return
 	}
 	// Aging speeds up when diseased
-	if frame[y][x].Diseased {
+	if me.Diseased {
 		frame[y][x].Age = frame[y][x].Age * 3 / 2
 		if rand.Intn(2) == 1 {
 			frame[y][x].Diseased = false // Cured! (50% chance)
@@ -33,7 +35,7 @@ func simulatePos(x, y int) {
 	}
 
 	// Reproduction
-	if frame[y][x].ReproductionValue > reproductionThreshold {
+	if me.ReproductionValue > reproductionThreshold {
 		var newX, newY int
 		foundPos := false
 		for x2 := -1; x2 <= 1; x2++ {
@@ -77,16 +79,16 @@ func simulatePos(x, y int) {
 		newX = rand.Intn(3) - 1
 		newY = rand.Intn(3) - 1
 	}
-	if frame[newY][newX].Color == groundColor {
+	newCell := frame[newY][newX]
+	if newCell.Color == groundColor {
 		if out[newY][newX].Color != r.Blank {
 			out[y][x] = out[newY][newX]
 		} else {
 			out[y][x].Color = groundColor
 		}
 		out[newY][newX] = frame[y][x]
-	} else if frame[newY][newX].Color != waterColor && frame[newY][newX].Color != frame[y][x].Color {
+	} else if newCell.Color != waterColor && newCell.Color != me.Color {
 		// Fighting
-		me := frame[y][x]
 		enemy := frame[newY][newX]
 		if me.Strength > enemy.Strength {
 			out[newY][newX] = me
